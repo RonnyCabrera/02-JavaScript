3163083a4d80bd77daf15ddf1e8fdcc56fff58ca
Ronny Cabrera <ronny-38@hotmail.com>
2018-05-08 22:45:14 -0500

    Arreglando Codigo

--- JavaScript/01-variables.js
+++ JavaScript/01-variables.js
@@ -1,78 +1,83 @@
-console.log('Hola Mundo');
-var mutar;
+console.log('Hola mundo!');
+var mutar; // undefined
 var nombre = 'Ronny';
 mutar = nombre;
-var edad = 23;
+var edad = 22;
 mutar = edad;
-var deuda = 223.445;
-mutar = deauda;
-var creado = false;
-var estoyVacio = null;
-var noEstoyDefinido = undefined;
-
+var deudas = 223.438;
+mutar = deudas;
+var fechaNacimiento = new Date();
+mutar = fechaNacimiento;
+var casado = false;
+muter = casado;
+var estoyVacio = null; // false
+var noEstoyDefinido = undefined; // false
+// null undefined 0 -1 1
+// -1 -> true 0 -> false 1 -> true
 if (estoyVacio) {
     console.log('Verdadero');
 } else {
     console.log('Falso');
 }
 
+// Json
+// ctrl + a = seleccionar
+// ctrl + alt + l = formatear
 var usuario = {
-    'nombre':
-'Ronny',
-    'apellido':
-'Cabrera',
-    //'edad':
-//'23',
-    edad: sumarDosNumeros(3,3),
-    //imprimirEnConsola: function () {
-        //console.log(this.nombre + ' ' + this.apellido + ' ' + this.edad);
-    //}
+    "nombre": "Ronny",
+    apellido: 'Cabrera',
+    edad: sumarDosNumeros(14, 14),
+    imprimirEnConsola: sumarDosNumeros
+    // imprimirEnConsola: function(){
+    //     console.log(this.nombre + ' ' + this.apellido + ' ' + this.edad);
+    // }
 };
-console.log(usuario.nombre);
-console.log(usuario);
-
+console.log(usuario.nombre); // "Adrian"
+console.log(usuario); // ????
 delete usuario.edad;
 console.log(usuario);
-
-usuario.cedula = '1723029662';
+usuario.cedula = '1723029665';
 console.log(usuario);
-
-usuario.mascota = {};
-usuario.mascota.nombre = 'Spod';
+usuario.mascotas = {};
+usuario.mascotas.nombre = 'Spod';
 console.log(usuario);
+console.log(typeof nombre); // "string"
+console.log(typeof edad); // "number"
+console.log(typeof casado); // "boolean"
+console.log(typeof fechaNacimiento); // "object"
+console.log(typeof usuario); // "object"
 
-console.log(typeof nombre);
-console.log(typeof edad);
-console.log(typeof usuario);
+// no se puede definir nombre en undefined
 
-function sumarDosNumeros(numeroUno, numeroDos){
+function sumarDosNumeros(numeroUno, numeroDos) {
     return numeroUno + numeroDos;
 }
-mutar = sumarDosNumeros();
-console.log(sumarDosNumeros(1,2));
 
-//Funcion Anonima
-var sumarDosNumerosVersionDos = function (numeroUno, numeroDos) {
+console.log(sumarDosNumeros(1, 2, 3, 4, 5, 6, 7)); // 3
+var sumarDosNumerosVersionDos = function noImportaSiEscriboElNombre(numeroUno, numeroDos) {
     return numeroUno + numeroDos;
 }
-console.log(sumarDosNumerosVersionDos(2,2));
-mutar = sumarDosNumerosVersionDos;
+// Funcion anonima
+var sumarDosNumerosVersionTres = function (numeroUno, numeroDos) {
+    return numeroUno + numeroDos;
+}
+mutar = sumarDosNumeros;
 
-console.log(usuario.imprimirEnConsola);
+console.log(usuario.imprimirEnConsola); //
 
 var arreglo = [
     1,
     2,
     3,
-    'Ronny',
+    "Ronny",
     2.2,
     true,
     undefined,
     null,
     {
-        nombre:'Eli'
+        nombre:"Santiago"
     },
-    sumarDosNumeros(2,6),
+    sumarDosNumeros(1,2),
     sumarDosNumeros,
     [1,2,3,true]
 ];
